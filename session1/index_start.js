f65ba704d0f52f160528316e3cf4ae1e482f98bb
Joanne Chen <joanne_r_chen@yahoo.com>
2020-11-06 11:50:26 +0000

    session 1: 1,2,3,6 finished

--- session1/index_start.js
+++ session1/index_start.js
@@ -3,7 +3,15 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  const strRev = str.split("").reverse().join("");
+  return strRev
+
+  //or use decrement for loop method:
+  // var newString = "";
+  // for (var i = str.length - 1; i >= 0; i--) {
+  //     newString += str[i];
+  // }
+  // return newString;
 }
 
 
@@ -12,7 +20,11 @@ function reverseString(str) {
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  const strRev = str.split("").reverse().join("");
+  return str === strRev? true : false;
+
+}
 
 
 
@@ -20,7 +32,10 @@ function isPalindrome(str) {}
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) {}
+function reverseInt(int) {
+  const intRev = int.toString().split("").reverse().join("")
+  return Number(intRev);
+}
 
 
 
@@ -40,11 +55,26 @@ function maxCharacter(str) {}
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  let output = ""
+  for (let i = 1; i <= 100; i++) {
+
+    if( i % 15 === 0) {
+      output += "FizzBuzz"
+    } else if (i % 3 === 0){
+      output += "Fizz"
+    } else if ( i % 2 === 0) {
+      output += "Buzz"
+    } else {
+      output += i
+    } 
+  }
+  return output
+}
 
 
 
 // Call Function
-const output = reverseString('hello');
+const output = fizzBuzz();
 
 console.log(output);
